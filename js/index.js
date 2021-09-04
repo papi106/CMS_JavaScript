47f81b05bc2d70e80aacb2b493f92ad3800467a6
Patrick Pacurar <patrickpacurar@yahoo.com>
2021-09-04 20:59:30 +0300

    added a code to supress an error changed values for gender implemented the adding new employee and local storage

--- js/index.js
+++ js/index.js
@@ -1,23 +1,25 @@
 
-function AppendTable(employees) {
-    tableContent = '';
+window.onload = () =>{
 
-    employees.forEach(e => {
-        tableContent += 
-        `<tr employee-id=${e.employeeId}>
-        <td>${e.picture}</td>
-        <td>${e.lastName}</td>
-        <td>${e.firstName}</td>
-        <td>${e.email}</td>
-        <td>${e.gender}</td>
-        <td>${e.birthDate}</td>
-        <td class="stergere"><img src="/images/trash.svg"></td>
-        </tr>`
-    });
-    
-    document.getElementById("table-employees").innerHTML = tableContent;
+    document.getElementById("add-employee-button").addEventListener("click", AddEmployee, false);
+
+    localStorage.setItem('employees', JSON.stringify([]));
+    localStorage.setItem('employeeNextId', JSON.stringify(0));
 }
 
+function AppendTable(employee) {
+    tableContent = `<tr employee-id=${employee.employeeId}>
+    <td>${employee.picture}</td>
+    <td>${employee.lastName}</td>
+    <td>${employee.firstName}</td>
+    <td>${employee.email}</td>
+    <td>${employee.gender}</td>
+    <td>${employee.birthDate}</td>
+    <td class="stergere"><img src="/images/trash.svg"></td>
+    </tr>`
+    console.log(employee);
+    document.getElementById("table-employees").innerHTML += tableContent;
+}
 
 function AddEmployee() {
     lastName = document.getElementById("last-name").value;
@@ -28,15 +30,24 @@ function AddEmployee() {
     picture = document.getElementById("picture-input").value;
 
 
-    employeeId = JSON.parse(localStorage.getItem('employeeInitilId'));
+    employeeId = JSON.parse(localStorage.getItem('employeeNextId'));
     allEmployees =  JSON.parse(localStorage.getItem('employees'));
 
     newEmployee = new Employee(employeeId++, lastName, firstName, email, gender, birthDate, picture);
     allEmployees.push(newEmployee);
 
-    localStorage.setItem('employeeInitialId', JSON.stringify(employeeId));
+    localStorage.setItem('employeeNextId', JSON.stringify(employeeId));
     localStorage.setItem('employees', JSON.stringify(allEmployees));
 
-    AppendTable(allEmployees);
+    AppendTable(newEmployee);
 }
 
+function Employee(employeeId, lastName, firstName, email, gender, birthDate, picture) {
+    this.employeeId = employeeId;
+    this.lastName = lastName;
+    this.firstName = firstName;
+    this.email = email;
+    this.birthDate = birthDate;
+    this.gender = gender;
+    this.picture= picture;
+}
