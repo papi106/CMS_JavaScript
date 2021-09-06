1e3f1207def0296b6b1cfe65651fd709ffd804e5
Patrick Pacurar <patrickpacurar@yahoo.com>
2021-09-06 21:00:37 +0300

    added search bar filter

--- js/index.js
+++ js/index.js
@@ -265,4 +265,23 @@ function sortTable() {
     });
 }
 
+//Search employee by string (search bar)
+function searchEmployee() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("search-employee");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("table-employees");
+    tr = table.getElementsByTagName("tr");
 
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[1];
+        if (td) {
+          txtValue = td.textContent || td.innerText;
+          if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            tr[i].style.display = "";
+          } else {
+            tr[i].style.display = "none";
+          }
+        }
+    }
+}
